b6e9391d678c923dfa9302a68afdcfc75bb22caf
Ashot Arutyunyan <jcl9595@gmail.com>
2017-03-24 17:50:17 -0400

    fixed relationship between user and articles

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,20 @@ class CommentsController < ApplicationController
 
 	def create
 		@article = Article.find(params[:article_id])
-		@comment = @article.comments.create(comment_params)
+		@comment = @article.comments.build(comment_params)
+
+		@comment.user_id = current_user.id
+
+		@comment.save
+
+
+		redirect_to article_path(@article)
+	end
+
+	def destroy
+		@article = Article.find(params[:article_id])
+		@comment = @article.comments.find(params[:id])
+		@comment.destroy
 
 		redirect_to article_path(@article)
 	end
